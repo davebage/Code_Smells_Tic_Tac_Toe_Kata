3377b1481602d0f3cf746e14960377237fd7e757
Dave Bage <david_bage@hotmail.com>
2022-10-28 18:29:01 +0100

    Move Tile to own file

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -5,35 +5,6 @@ using System.Runtime.InteropServices;
 
 namespace TicTacToe
 {
-    public class Tile
-    {
-        private readonly Token _symbol;
-        private readonly Coordinate _coordinate;
-
-        public Tile(Token symbol, Coordinate coordinate)
-        {
-            _coordinate = coordinate;
-            _symbol = symbol;
-        }
-
-        public bool CompareCoordinate(Coordinate other)
-        {
-            return _coordinate.Equals(other);
-        }
-
-        public bool CompareSymbol(Tile other)
-        {
-            if(other == null) return false;
-            return other._symbol == _symbol;
-        }
-
-        public Token GetToken()
-        {
-            return _symbol;
-        }
-
-    }
-
     public class Board
     {
         private readonly List<Tile> _plays = new List<Tile>();
---
+++ TicTacToe/Tile.cs
@@ -0,0 +1,31 @@
+﻿namespace TicTacToe
+{
+    public class Tile
+    {
+        private readonly Token _symbol;
+        private readonly Coordinate _coordinate;
+
+        public Tile(Token symbol, Coordinate coordinate)
+        {
+            _coordinate = coordinate;
+            _symbol = symbol;
+        }
+
+        public bool CompareCoordinate(Coordinate other)
+        {
+            return _coordinate.Equals(other);
+        }
+
+        public bool CompareSymbol(Tile other)
+        {
+            if(other == null) return false;
+            return other._symbol == _symbol;
+        }
+
+        public Token GetToken()
+        {
+            return _symbol;
+        }
+
+    }
+}
