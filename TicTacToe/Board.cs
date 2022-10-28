5b277996d416d16192b6e6d301191a2d91930d19
Dave Bage <david_bage@hotmail.com>
2022-10-28 18:29:37 +0100

    move Board to own file

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        private readonly List<Tile> _plays = new List<Tile>();
+
+        public Tile TileAt(Coordinate coordinate)
+        {
+            return _plays.FirstOrDefault(tile => tile.CompareCoordinate(coordinate));
+        }
+
+        public Tile TileAt(int row, int column)
+        {
+            return TileAt(new Coordinate(row, column));
+        }
+
+        public void AddTileAt(Token symbol, Coordinate coordinate)
+        {
+            _plays.Add(new Tile(symbol, coordinate));
+        }
+    }
+}
--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -1,30 +1,8 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 namespace TicTacToe
 {
-    public class Board
-    {
-        private readonly List<Tile> _plays = new List<Tile>();
-
-        public Tile TileAt(Coordinate coordinate)
-        {
-            return _plays.FirstOrDefault(tile => tile.CompareCoordinate(coordinate));
-        }
-
-        public Tile TileAt(int row, int column)
-        {
-            return TileAt(new Coordinate(row, column));
-        }
-
-        public void AddTileAt(Token symbol, Coordinate coordinate)
-        {
-            _plays.Add(new Tile(symbol, coordinate));
-        }
-    }
-
     public class Game
     {
         private Token _lastSymbol = Token.O;
